6c4301e0f8191b16a49383eb0f07cfcef168bfec
HawaiiDev <hawaiilive@outlook.com>
2022-02-16 21:30:03 +0100

    Test fetches for network testing

--- RaspberryPI/test.py
+++ RaspberryPI/test.py
@@ -1,4 +1,10 @@
 import requests
+from dotenv import dotenv_values
 
 r = requests.get("https://www.google.com")
+print("Google Status: " + str(r.status_code))
+
+config = dotenv_values(".env")
+url = config["SERVER_URL"]
+r = requests.get(url)
 print(r.status_code)
