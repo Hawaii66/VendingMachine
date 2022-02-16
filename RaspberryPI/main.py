323a714b2591254058437512c157aa70fc6bc5e8
HawaiiDev <hawaiilive@outlook.com>
2022-02-16 21:30:20 +0100

    Low_mode with force request

--- RaspberryPI/main.py
+++ RaspberryPI/main.py
@@ -3,6 +3,7 @@ import parse_command
 import machine
 import fetch
 from time import sleep
+import time
 from dotenv import dotenv_values
 
 def main():
@@ -10,16 +11,18 @@ def main():
     sleepTime = int(config["SLEEP_TIME"])
     machineID = config["MACHINE_ID"]
 
-    fetch.test()
-    
+    print("Starting up with a test")
+
+    #fetch.test()
+
     print(f"Starting with index: {machineID}")
 
     vend = machine.vending_machine("1644692876708:724184:machine",[
         {
             "index":0,
             "amount":3,
-            "x":230,
-            "y":1760
+            "x":240,
+            "y":1900
         }
     ])
     vend.init_arduino()
@@ -28,7 +31,7 @@ def main():
     while True:
         order = fetch.get_one_order()
         if order == False:
-            sleep(sleepTime)
+            low_mode(vend, sleepTime)
         else:
             exec_order(vend, order)
 
@@ -38,5 +41,18 @@ def exec_order(vend, order):
     vend.vend_index(index)
 
 
+def low_mode(vend, elap):
+    start_time = time.time()
+    count = start_time
+
+    while count < start_time + elap:
+        ser = parse_command.get_string(vend.get_arduino())
+        if "Send Req" in ser:
+            break
+        
+        count = time.time()
+        if count >= start_time + elap:
+            break
+    
 if __name__ == "__main__":
     main()
