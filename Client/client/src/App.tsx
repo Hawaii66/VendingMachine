f45c3345acc9cb2185b7918dee5e0260db636a65
Sebastian Ahlman <hawaiilive@outlook.com>
2022-02-19 10:21:06 +0100

    Init client

--- Client/client/src/App.tsx
+++ Client/client/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Swish from './Components/Swish';
 
 function App() {
 	return (
 		<div className="App">
-			
+			<Swish/>
 		</div>
 	);
 }
---
+++ Client/client/src/Components/Swish.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Swish() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Swish
